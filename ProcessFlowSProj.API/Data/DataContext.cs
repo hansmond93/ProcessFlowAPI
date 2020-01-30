fb78e72a1bb5dcb74fdf2e8c7dbae292e6fbab5e
Adesola <hansmond93@gmail.com>
2020-01-30 15:59:36 +0100

    First Commit

---
+++ ProcessFlowSProj.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProcessFlowSProj.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessFlowSProj.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ ProcessFlowSProj.API/Entities/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessFlowSProj.API.Entities
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
